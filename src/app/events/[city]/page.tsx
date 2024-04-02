7f243346f0c9626fd5ce620f68ecf0fce33c58e5
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-02 14:10:47 +0200

    adding event card list

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,4 @@
+import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
 import { EventoEvent } from "@/lib/types";
 
@@ -25,9 +26,7 @@ async function EventsPage({ params }: EventsPageProps) {
           `Events in ${city.charAt(0).toLocaleUpperCase() + city.slice(1)}`}
       </H1>
 
-      {events.map((event) => (
-        <section key={event.id}>{event.name}</section>
-      ))}
+      <EventsList events={events} />
     </main>
   );
 }
