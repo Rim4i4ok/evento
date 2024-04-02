7033047208be5c0247adfc12311d4828170991cb
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-02 14:03:19 +0200

    adding evento event type response

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,4 +1,5 @@
 import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
 
 type EventsPageProps = {
   params: {
@@ -6,8 +7,16 @@ type EventsPageProps = {
   };
 };
 
-function EventsPage({ params }: EventsPageProps) {
+async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+
+  const events: EventoEvent[] = await response.json();
+  console.log(events);
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1>
@@ -15,6 +24,10 @@ function EventsPage({ params }: EventsPageProps) {
         {city !== "all" &&
           `Events in ${city.charAt(0).toLocaleUpperCase() + city.slice(1)}`}
       </H1>
+
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   );
 }
