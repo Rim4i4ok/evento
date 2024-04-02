5ddd4655f239994f123320a90bc9c33b1625a01a
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-02 12:33:27 +0200

    adding smooth animation for active link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Logo from "./logo";
+import { motion } from "framer-motion";
 
 const routes = [
   {
@@ -23,17 +24,27 @@ function Header() {
     <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
 
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className="h-full">
+        <ul className="flex gap-x-6 text-sm h-full">
           {routes.map((route) => (
             <li
               key={route.path}
-              className={clsx("hover:text-white transition", {
-                "text-white": activePathname === route.path,
-                "text-white/50": activePathname !== route.path,
-              })}
+              className={clsx(
+                "hover:text-white flex items-center relative transition",
+                {
+                  "text-white": activePathname === route.path,
+                  "text-white/50": activePathname !== route.path,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+
+              {activePathname === route.path && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="bg-accent h-1 w-full absolute bottom-0"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
