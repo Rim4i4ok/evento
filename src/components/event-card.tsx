290d3a640e2294b92403d4e1f18b5e95ea980ccd
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-02 14:37:16 +0200

    adding date to card

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -6,10 +6,10 @@ type EventCardProps = {
 };
 
 function EventCard({
-  event: { imageUrl, name, organizerName, location },
+  event: { imageUrl, name, organizerName, location, date },
 }: EventCardProps) {
   return (
-    <section className="h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden flex flex-col flex-1 basis-80">
+    <section className="h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden flex flex-col flex-1 basis-80 relative">
       <Image
         className="h-[60%] object-fill"
         src={imageUrl}
@@ -23,6 +23,19 @@ function EventCard({
         <p className="italic text-white/75">{organizerName}</p>
         <p className="text-sm text-white/50 mt-4">{location}</p>
       </div>
+
+      <section className="absolute flex flex-col justify-center items-center left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
+        <p className="text-xl font-bold -mb-[5px]">
+          {new Date(date).toLocaleDateString("en-US", {
+            day: "2-digit",
+          })}
+        </p>
+        <p className="text-xs uppercase text-accent">
+          {new Date(date).toLocaleDateString("en-US", {
+            month: "short",
+          })}
+        </p>
+      </section>
     </section>
   );
 }
