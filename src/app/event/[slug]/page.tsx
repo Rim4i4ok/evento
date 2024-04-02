a635de5f9dd4118c5e62c3950b2e1b2b16c9c05b
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-02 23:00:37 +0200

    showing event image

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,5 +1,43 @@
-function EventPage() {
-  return <main>Event Page</main>;
+import Image from "next/image";
+
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+async function EventPage({ params: { slug } }: EventPageProps) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+
+  return (
+    <main>
+      <section className="relative h-[361px] overflow-hidden">
+        <Image
+          className="object-cover blur-3xl z-0"
+          src={event.imageUrl}
+          alt="Event background image"
+          fill
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          quality={50}
+          priority
+        />
+
+        <div className="relative z-1">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+          />
+        </div>
+      </section>
+
+      <div></div>
+    </main>
+  );
 }
 
 export default EventPage;
