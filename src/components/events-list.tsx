62e045a93e0e86c71cfda99eb39b49e87a2acdcb
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-03 16:20:24 +0200

    adding revalidation for events list

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -7,7 +7,12 @@ type EventsListProps = {
 
 async function EventsList({ city }: EventsListProps) {
   const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
+    {
+      next: {
+        revalidate: 5 * 60,
+      },
+    }
   );
 
   const events: EventoEvent[] = await response.json();
