7034737a70d5af3edc51fa3d75f8ff4442012b81
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-03 17:28:03 +0200

    adding motion to cars list

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,18 +1,43 @@
+"use client";
+
 import { EventoEvent } from "@/lib/types";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 
 type EventCardProps = {
   event: EventoEvent;
 };
 
+const MotionLink = motion(Link);
+
 function EventCard({
   event: { imageUrl, name, organizerName, location, date, slug },
 }: EventCardProps) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.5 1"],
+  });
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
+
   return (
-    <Link
+    <MotionLink
+      ref={ref}
       className="flex-1 basis-80 h-[380px] max-w-[500px]"
       href={`/event/${slug}`}
+      style={{
+        // @ts-ignore
+        scale: scaleProgress,
+        // @ts-ignore
+        opacity: opacityProgress,
+      }}
+      initial={{
+        scale: 0.8,
+        opacity: 0,
+      }}
     >
       <section className="w-full h-full  bg-white/[3%] rounded-xl overflow-hidden flex flex-col  relative state-effects">
         <Image
@@ -42,7 +67,7 @@ function EventCard({
           </p>
         </section>
       </section>
-    </Link>
+    </MotionLink>
   );
 }
 
