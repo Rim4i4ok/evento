b26169af9190b3bad72c09a621e96311553cff16
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-03 16:01:39 +0200

    adding custom loading skeletons

--- src/app/events/[city]/loading.tsx
+++ src/app/events/[city]/loading.tsx
@@ -1,5 +1,13 @@
+import SkeletonCard from "@/components/skeleton-card";
+
 function Loading() {
-  return <div>Loading events...</div>;
+  return (
+    <div className="flex flex-wrap justify-center max-w-[1100px] mx-auto px-[20px] py-[20px] gap-20">
+      {Array.from({ length: 6 }).map((_, index) => (
+        <SkeletonCard key={index} />
+      ))}
+    </div>
+  );
 }
 
 export default Loading;
