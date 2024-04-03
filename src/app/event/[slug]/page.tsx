db9db59ec383b0b610447704d91004f7321e619f
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-03 13:44:03 +0200

    adding styles for event

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import H1 from "@/components/h1";
 import Image from "next/image";
 
 type EventPageProps = {
@@ -14,7 +15,7 @@ async function EventPage({ params: { slug } }: EventPageProps) {
 
   return (
     <main>
-      <section className="relative h-[361px] overflow-hidden">
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
         <Image
           className="object-cover blur-3xl z-0"
           src={event.imageUrl}
@@ -25,13 +26,36 @@ async function EventPage({ params: { slug } }: EventPageProps) {
           priority
         />
 
-        <div className="relative z-1">
+        <div className="relative z-1 flex flex-col lg:flex-row gap-6 lg:gap-16">
           <Image
+            className="rounded-xl border-2 border-white/50 object-cover"
             src={event.imageUrl}
             alt={event.name}
             width={300}
             height={201}
           />
+
+          <div className="flex flex-col ">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+
+            <button className="text-lg bg-white/20 capitalize mt-5 lg:mt-auto w-[95vm] sm:w-full py-2 rounded-md border-2 border-white/10 hover:scale-x-105 transition focus:scale-105 active:scale-[1.02]">
+              Get tickets
+            </button>
+          </div>
         </div>
       </section>
 
