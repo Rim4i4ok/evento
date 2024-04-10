cc5c622d83950ab030af109edd49262fe3ebe547
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-10 14:06:06 +0200

    adding custom error page

---
+++ src/app/error.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import H1 from "@/components/h1";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Optionally log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="flex h-full flex-col items-center justify-center py-24">
+      <H1 className="mb-5">Something went wrong!</H1>
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+          // Attempt to recover by trying to re-render the invoices route
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </main>
+  );
+}
