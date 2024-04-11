dfb8c54c58a5e3b4f16e62c76d92360aee997939
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-11 10:38:57 +0200

    renaming db.ts -> server-utils.ts

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,4 +1,4 @@
-import { getEvents } from "@/lib/db";
+import { getEvents } from "@/lib/server-utils";
 import EventCard from "./event-card";
 import PaginationControls from "./pagination-controls";
 import { DB_STEP_ITEMS } from "@/lib/constants";
