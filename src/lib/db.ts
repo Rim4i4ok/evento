7e92f235c4dbc4aa4545d457c9592c92aab7b6e5
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-11 10:30:02 +0200

    adding data cache to prisma requests

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,34 +2,37 @@ import { notFound } from "next/navigation";
 import prisma from "./prisma";
 import { capitalize } from "./utils";
 import { DB_STEP_ITEMS } from "./constants";
+import { unstable_cache } from "next/cache";
 
-export async function getEvents(city: string, page: number = 1) {
-  const events = await prisma.eventoEvent.findMany({
-    where: {
-      city: city === "all" ? undefined : capitalize(city),
-    },
-    orderBy: {
-      date: "asc",
-    },
-    take: DB_STEP_ITEMS,
-    skip: (page - 1) * DB_STEP_ITEMS,
-  });
-
-  let totalCount = 0;
-  if (city === "all") {
-    totalCount = await prisma.eventoEvent.count();
-  } else {
-    totalCount = await prisma.eventoEvent.count({
+export const getEvents = unstable_cache(
+  async (city: string, page: number = 1) => {
+    const events = await prisma.eventoEvent.findMany({
       where: {
         city: city === "all" ? undefined : capitalize(city),
       },
+      orderBy: {
+        date: "asc",
+      },
+      take: DB_STEP_ITEMS,
+      skip: (page - 1) * DB_STEP_ITEMS,
     });
+
+    let totalCount = 0;
+    if (city === "all") {
+      totalCount = await prisma.eventoEvent.count();
+    } else {
+      totalCount = await prisma.eventoEvent.count({
+        where: {
+          city: city === "all" ? undefined : capitalize(city),
+        },
+      });
+    }
+
+    return { events, totalCount };
   }
+);
 
-  return { events, totalCount };
-}
-
-export async function getEvent(slug: string) {
+export const getEvent = unstable_cache(async (slug: string) => {
   const event = await prisma.eventoEvent.findUnique({
     where: {
       slug: slug,
@@ -41,4 +44,4 @@ export async function getEvent(slug: string) {
   }
 
   return event;
-}
+});
